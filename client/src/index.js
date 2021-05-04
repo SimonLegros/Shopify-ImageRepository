25c135af066e9a44f2990d3da95d4af1aa878307
Simon Legros <simonlegros5@gmail.com>
2021-05-04 14:21:30 -0400

    make proxy link work on docker containers

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import axios from 'axios';
+// import axios from 'axios';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 ReactDOM.render(
   <React.StrictMode>
